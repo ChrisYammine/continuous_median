aa49fcb816889e83a783e61a078ff0ffd7b6e5d0
Christopher Yammine <christophery@mogo.ca>
2016-11-04 00:54:05 -0700

    Getting closer? also go fmt

--- main.go
+++ main.go
@@ -1,68 +1,83 @@
 package main
 
 import (
-  "os"
-  "bufio"
-  "fmt"
-  "container/heap"
-  "strconv"
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func addNumber(x int, lowers *MaxHeap, highers *MinHeap) {
-  if lowers.Len() == 0 || x < lowers.Peek() {
-    heap.Push(lowers, x)
-  } else {
-    heap.Push(highers, x)
-  }
+	if lowers.Len() == 0 {
+		heap.Push(lowers, x)
+		return
+	}
+
+	highestFromLowers := heap.Pop(lowers).(int)
+	heap.Push(lowers, highestFromLowers)
+	if x < highestFromLowers {
+		heap.Push(lowers, x)
+	} else {
+		heap.Push(highers, x)
+	}
 }
 
 func rebalance(lowers *MaxHeap, highers *MinHeap) {
-  if lowers.Len() < highers.Len() {
-    if highers.Len() - lowers.Len() >= 2 {
-      heap.Push(lowers, heap.Pop(highers))
-    }
-  } else {
-    if lowers.Len() - highers.Len() >= 2 {
-      heap.Push(highers, heap.Pop(lowers))
-    }
-  }
+	if lowers.Len() < highers.Len() {
+		if highers.Len()-lowers.Len() >= 2 {
+			heap.Push(lowers, heap.Pop(highers))
+		}
+	} else {
+		if lowers.Len()-highers.Len() >= 2 {
+			heap.Push(highers, heap.Pop(lowers))
+		}
+	}
 }
 
 func findMedian(lowers *MaxHeap, highers *MinHeap) float64 {
-  if lowers.Len() == highers.Len() {
-    return (float64(lowers.Peek()) + float64(highers.Peek())) / 2.0
-  }
-  if lowers.Len() < highers.Len() {
-    return float64(highers.Peek())
-  } else {
-    return float64(lowers.Peek())
-  }
+	if lowers.Len() == highers.Len() {
+		low := heap.Pop(lowers).(int)
+		high := heap.Pop(highers).(int)
+		heap.Push(lowers, low)
+		heap.Push(highers, high)
+		return (float64(low) + float64(high)) / 2.0
+	}
+	if lowers.Len() < highers.Len() {
+		high := heap.Pop(highers).(int)
+		heap.Push(highers, high)
+		return float64(highers.Peek())
+	} else {
+		low := heap.Pop(lowers).(int)
+		heap.Push(lowers, low)
+		return float64(lowers.Peek())
+	}
 }
 
 func main() {
-  scanner := bufio.NewScanner(os.Stdin)
-  scanner.Scan()
-  n, _ := strconv.Atoi(scanner.Text())
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
 
-  arr  := make([]int, n)
+	arr := make([]int, n)
 
-  lowers  := &MaxHeap{}
-  highers := &MinHeap{}
-  heap.Init(lowers)
-  heap.Init(highers)
+	lowers := &MaxHeap{}
+	highers := &MinHeap{}
+	heap.Init(lowers)
+	heap.Init(highers)
 
-  // Collecting our input
-  for i, _ := range(arr) {
-    scanner.Scan()
-    x, _ := strconv.Atoi(scanner.Text())
-    arr[i] = x
-  }
+	// Collecting our input
+	for i, _ := range arr {
+		scanner.Scan()
+		x, _ := strconv.Atoi(scanner.Text())
+		arr[i] = x
+	}
 
-  // Printing our medians
-  for _, x := range(arr) {
-    addNumber(x, lowers, highers)
-    rebalance(lowers, highers)
-    median := findMedian(lowers, highers)
-    fmt.Printf("%.2f\n", median)
-  }
+	// Printing our medians
+	for _, x := range arr {
+		addNumber(x, lowers, highers)
+		rebalance(lowers, highers)
+		median := findMedian(lowers, highers)
+		fmt.Printf("%.2f\n", median)
+	}
 }
--- min_heap.go
+++ min_heap.go
@@ -4,33 +4,33 @@ package main
 type MinHeap []int
 
 func (h MinHeap) Len() int {
-  return len(h)
+	return len(h)
 }
 
 func (h MinHeap) Less(i, j int) bool {
-  return h[i] < h[j]
+	return h[i] < h[j]
 }
 
 func (h MinHeap) Swap(i, j int) {
-  h[i], h[j] = h[j], h[i]
+	h[i], h[j] = h[j], h[i]
 }
 
 // Uses a pointer receiver because it modifies the slice's length & not just the contents
 func (h *MinHeap) Push(x interface{}) {
-  *h = append(*h, x.(int))
+	*h = append(*h, x.(int))
 }
 
 // Uses a pointer receiver because it modifies the slice's length & not just the contents
 func (h *MinHeap) Pop() interface{} {
-  old := *h
-  n   := len(old)
-  x   := old[n-1]
-  *h   = old[:n-1]
-  return x
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func (h *MinHeap) Peek() int {
-  heap := *h
-  n := len(*h)
-  return heap[n-1]
+	heap := *h
+	n := len(*h)
+	return heap[n-1]
 }
